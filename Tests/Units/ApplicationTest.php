980e0aa165e736a6f56fa0e2ebc89bbb71f39d17
Ricky <ricky.gamer000@gmail.com>
2020-05-20 22:19:49 -0400

    First Test

--- Tests/Units/ApplicationTest.php
+++ Tests/Units/ApplicationTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Units;
 
+use App\Helpers\App;
 use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
     public function testItCanGetInstanceOfApplication()
     {
-        self::assertInstanceOf(App::class, new App());
+        self::assertInstanceOf(App::class, new App);
     }
 }
