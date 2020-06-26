1e0c5e4bbc6ec2d1c57cb65181eb3555715b780d
Ricky <ricky.gamer000@gmail.com>
2020-06-26 16:17:57 -0400

    commented out logger tests in index.php

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require_once __DIR__ .'/vendor/autoload.php';
 
 require_once __DIR__ .'/Src/Exception/exception.php';
 
-$logger = new \App\Logger\Logger();
-$logger->log(\App\Logger\LogLevel::EMERGENCY, 'test emergency', ['exception'=>'exception occured.']);
-$logger->info('User made ok.',['id'=>5]);
+//$logger = new \App\Logger\Logger();
+//$logger->log(\App\Logger\LogLevel::EMERGENCY, 'test emergency', ['exception'=>'exception occured.']);
+//$logger->info('User made ok.',['id'=>5]);
