ea45ba0f85e6a6954d19fd568529add4e9a77f8b
Ricky <ricky.gamer000@gmail.com>
2020-06-26 17:34:55 -0400

    added database connection keys and credentials for database connection

--- Src/Configs/database.php
+++ Src/Configs/database.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-
+    'pdo' => [
+        'driver'        => 'mysql',
+        'host'          => 'localhost',
+        'db_name'       => 'bugout',
+        'db_username'   => 'root',
+        'db_password'   => 'root',
+        'default_fetch' => PDO::FETCH_OBJ,
+    ],
 ];
