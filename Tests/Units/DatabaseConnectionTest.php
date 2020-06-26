66c4877dfeccf589b77c19224aab085069f62f3e
Ricky <ricky.gamer000@gmail.com>
2020-06-26 17:36:59 -0400

    Created new DatabaseConnectionTest tests

--- Tests/Units/DatabaseConnectionTest.php
+++ Tests/Units/DatabaseConnectionTest.php
@@ -2,19 +2,41 @@
 
 namespace Tests\Units;
 
+use App\Contracts\DatabaseConnectionInterface;
+use App\Database\PDOConnection;
+use App\Exception\MissingArgumentException;
+use App\Helpers\Config;
 use PHPUnit\Framework\TestCase;
 
-class DatabaseConnectionTest extends TestCase 
+class DatabaseConnectionTest extends TestCase
 {
+
+    public function testItThrowMissingArgumentExceptionWithWrongCredentialKeys()
+    {
+        self::expectException(MissingArgumentException::class);
+        $credentials = [];
+        $pdoHandler = new PDOConnection($credentials);
+    }
 
     public function testItCanConnectToDatabaseWithPdoApi()
     {
-
+        $credentials = $this->getCredentials('pdo');
+        $pdoHandler = (new PDOConnection($credentials))->connect();
+        self::assertInstanceOf(DatabaseConnectionInterface::class, $pdoHandler);
+        return $pdoHandler;
     }
 
-    public function testItCanConnectToDatabaseWithMySqlApi()
+    /** @depends testItCanConnectToDatabaseWithPdoApi */
+    public function testItIsAValidPdoConnection(DatabaseConnectionInterface $handler)
     {
-        
-    }    
+        self::assertInstanceOf(\PDO::class, $handler->getConnection());
+    }
 
+    private function getCredentials(string $type)
+    {
+        return array_merge(
+            Config::get('database', $type),
+            ['db_name' => 'bugout_testing']
+        );
+    }
 }
